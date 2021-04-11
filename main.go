4e3c57fc2303fe5fac7710918a4d0b73be21ff7f
dacharat <dacharat.p@ku.th>
2021-04-11 17:20:52 +0700

    :octocat: gen wire

--- main.go
+++ main.go
@@ -1,8 +1,44 @@
 package main
 
-import "github.com/dacharat/wire-gin/di"
+import (
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/dacharat/wire-gin/di"
+	"github.com/dacharat/wire-gin/routers"
+	"github.com/dacharat/wire-gin/utils"
+	"github.com/gin-gonic/gin"
+	"github.com/pkg/errors"
+)
+
+func gracefullyShutdown(router *gin.Engine) {
+	server := &http.Server{
+		Addr:    ":8000",
+		Handler: router,
+	}
+
+	go func() {
+		if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			panic(errors.Wrap(err, "Cannot open socket"))
+		}
+	}()
+
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	<-quit
+
+	ctx, cancel := utils.GetContext()
+	defer cancel()
+
+	if err := server.Shutdown(ctx); err != nil {
+		panic(errors.Wrap(err, "Forced shutdown"))
+	}
+}
 
 func main() {
-	_ = di.New()
+	api := di.New()
 
+	gracefullyShutdown(routers.New(api))
 }
--- routers/routes.go
+++ routers/routes.go
@@ -1,11 +1,11 @@
-package rotuers
+package routers
 
 import (
 	"github.com/dacharat/wire-gin/products"
 	"github.com/gin-gonic/gin"
 )
 
-func New(h *products.ProductAPI) *gin.Engine {
+func New(h products.ProductAPI) *gin.Engine {
 	router := gin.New()
 
 	p := router.Group("/api/v1/products")
